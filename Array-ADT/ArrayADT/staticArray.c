1ffea321f12afa1dd8e9eb07863ee61a913e8776
Shruti3004 <mail2shruti.ag@gmail.com>
2021-02-01 00:41:31 +0530

    Added delete function

--- Array-ADT/ArrayADT/staticArray.c
+++ Array-ADT/ArrayADT/staticArray.c
@@ -41,11 +41,25 @@ void insert(struct Array *arr, int index, int x)
     }
 }
 
+// Delete Function
+void delete (struct Array *arr, int index)
+{
+    if (index < arr->length)
+    {
+        for (int i = index; i < arr->length - 1; i++)
+        {
+            arr->A[i] = arr->A[i + 1];
+        }
+        arr->length--;
+    }
+}
+
 int main()
 {
     struct Array arr = {{2, 3, 4, 5, 6}, 10, 5};
     // append(&arr, 10);
-    insert(&arr, 2, 10);
+    // insert(&arr, 2, 10);
+    delete (&arr, 2);
     display(arr);
     return 0;
 }
