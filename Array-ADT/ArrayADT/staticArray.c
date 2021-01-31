654347ff4c1e1e5cc3c5e4ac8dbd4b03d8fb386d
Shruti3004 <mail2shruti.ag@gmail.com>
2021-02-01 00:10:33 +0530

    Added append and insert function

--- Array-ADT/ArrayADT/staticArray.c
+++ Array-ADT/ArrayADT/staticArray.c
@@ -3,20 +3,49 @@
 
 struct Array
 {
-    int A[20];
+    int A[10];
     int size;
     int length;
 };
+
+// Display Function
 void display(struct Array arr)
 {
     for (int i = 0; i < arr.length; i++)
     {
         printf("%d ", arr.A[i]);
     }
+    printf("\nLength : %d", arr.length);
 }
+
+// Append Function
+void append(struct Array *arr, int x)
+{
+    if (arr->length < arr->size)
+    {
+        arr->A[arr->length++] = x;
+    }
+}
+
+// Insert Function
+void insert(struct Array *arr, int index, int x)
+{
+    if (index >= 0 && index <= arr->length)
+    {
+        for (int i = arr->length; i > index; i--)
+        {
+            arr->A[i] = arr->A[i - 1];
+        }
+        arr->A[index] = x;
+        arr->length++;
+    }
+}
+
 int main()
 {
-    struct Array arr = {{2, 3, 4, 5, 6}, 20, 5};
+    struct Array arr = {{2, 3, 4, 5, 6}, 10, 5};
+    // append(&arr, 10);
+    insert(&arr, 2, 10);
     display(arr);
     return 0;
 }
