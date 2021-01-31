51e13d6e8b570cf0e61f727fe9439a4e394e6da5
Shruti3004 <mail2shruti.ag@gmail.com>
2021-02-01 01:20:13 +0530

    Improvement in Linear Search

--- Array-ADT/ArrayADT/operations.c
+++ Array-ADT/ArrayADT/operations.c
@@ -54,6 +54,14 @@ void delete (struct Array *arr, int index)
     }
 }
 
+void swap(int *x, int *y)
+{
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
 // Linear Search
 int linearSearch(struct Array arr, int key)
 {
@@ -67,14 +75,35 @@ int linearSearch(struct Array arr, int key)
     return -1;
 }
 
+// Improvement in Linear Search
+int impLinearSearch(struct Array *arr, int key)
+{
+    for (int i = 0; i < arr->length; i++)
+    {
+        if (arr->A[i] == key)
+        {
+            // Transposition
+            // swap(&arr->A[i], &arr->A[i - 1]);
+            // return i - 1;
+            // Move to Front/ Move to head
+            swap(&arr->A[i], &arr->A[0]);
+            return 0;
+        }
+    }
+    return -1;
+}
+
 int main()
 {
     struct Array arr = {{2, 3, 4, 5, 6}, 10, 5};
     // append(&arr, 10);
     // insert(&arr, 2, 10);
     // delete (&arr, 0);
-    int lSearch = linearSearch(arr, 5);
-    printf("%d", lSearch);
     // display(arr);
+
+    // int lSearch = linearSearch(arr, 4);
+    // printf("%d", lSearch);
+
+    printf("%d", impLinearSearch(&arr, 6));
     return 0;
 }
