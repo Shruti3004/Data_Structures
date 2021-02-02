2064bdaf890e629a53a205982d7e56f458962f71
Shruti3004 <mail2shruti.ag@gmail.com>
2021-02-02 23:30:32 +0530

    Added the function forMerge sort

--- Array-ADT/ArrayADT/operations.c
+++ Array-ADT/ArrayADT/operations.c
@@ -264,9 +264,34 @@ void rearrange(struct Array *arr)
     }
 }
 
+// Merge Sort
+struct Array *merge(struct Array arr1, struct Array arr2)
+{
+    int i = 0, j = 0, k = 0;
+    struct Array *arr3 = (struct Array *)malloc(sizeof(struct Array));
+    while (i < arr1.length && j < arr2.length)
+    {
+        if (arr1.A[i] < arr2.A[j])
+            arr3->A[k++] = arr1.A[i++];
+        else
+            arr3->A[k++] = arr2.A[j++];
+    }
+    for (; i < arr1.length; i++)
+        arr3->A[k++] = arr1.A[i];
+    for (; j < arr2.length; j++)
+        arr3->A[k++] = arr2.A[j];
+    arr3->length = arr1.length + arr2.length;
+    arr3->size=10;
+    return arr3;
+}
+
 int main()
 {
     struct Array arr = {{2, -3, 25, 10, -15, -7}, 10, 6};
+    struct Array arr1 = {{2, 6, 10, 15, 25}, 10, 5};
+    struct Array arr2 = {{3, 4, 7, 18, 20}, 10, 5};
+    struct Array *arr3;
+
     // append(&arr, 10);
     // insert(&arr, 2, 10);
     // delete (&arr, 0);
@@ -297,7 +322,10 @@ int main()
 
     // printf("%d", isSorted(arr));
 
-    rearrange(&arr);
-    display(arr);
+    // rearrange(&arr);
+    // display(arr);
+
+    arr3=merge(arr1,arr2);
+    display(*arr3);
     return 0;
 }
