f08279b4c4840e0e654f7cea47d69446568c260f
Shruti3004 <mail2shruti.ag@gmail.com>
2021-02-02 23:08:48 +0530

    Added the function forInsertion sort, isSorted, rearrange positives and negatives

--- Array-ADT/ArrayADT/operations.c
+++ Array-ADT/ArrayADT/operations.c
@@ -208,6 +208,7 @@ void Reverse(struct Array *arr)
     }
 }
 
+// Reverse2- (2nd Method)
 void Reverse2(struct Array *arr)
 {
     for (int i = 0, j = arr->length - 1; i < j; i++, j--)
@@ -216,9 +217,56 @@ void Reverse2(struct Array *arr)
     }
 }
 
+// Insert
+void InsertSort(struct Array *arr, int x)
+{
+    int i = arr->length - 1;
+    if (arr->length == arr->size)
+        return;
+    while (i >= 0 && arr->A[i] > x)
+    {
+        arr->A[i + 1] = arr->A[i];
+        i--;
+    }
+    arr->A[i + 1] = x;
+    arr->length++;
+}
+
+// Check if array is sorted or not
+int isSorted(struct Array arr)
+{
+    for (int i = 0; i < arr.length - 1; i++)
+    {
+        if (arr.A[i] > arr.A[i + 1])
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+// Rearranging Positives and negatives
+void rearrange(struct Array *arr)
+{
+    int i, j;
+    i = 0;
+    j = arr->length - 1;
+    while (i < j)
+    {
+        while (arr->A[i] < 0)
+            i++;
+        while (arr->A[j] >= 0)
+            j--;
+        if (i < j)
+        {
+            swap(&arr->A[i], &arr->A[j]);
+        }
+    }
+}
+
 int main()
 {
-    struct Array arr = {{2, 3, 4, 5, 6}, 10, 5};
+    struct Array arr = {{2, -3, 25, 10, -15, -7}, 10, 6};
     // append(&arr, 10);
     // insert(&arr, 2, 10);
     // delete (&arr, 0);
@@ -241,8 +289,15 @@ int main()
     // printf("%f", avg(arr));
 
     // display(arr);
-    Reverse2(&arr);
-    display(arr);
+    // Reverse2(&arr);
+    // display(arr);
 
+    // InsertSort(&arr, 1);
+    // display(arr);
+
+    // printf("%d", isSorted(arr));
+
+    rearrange(&arr);
+    display(arr);
     return 0;
 }
